dde6500ec65a0b81b826c8986f77fe51b3191a4c
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2024-06-04 14:42:50 +0200

    [psi] Encode results into field elements

--- psi/src/main.rs
+++ psi/src/main.rs
@@ -1,36 +1,39 @@
 use std::collections::HashSet;
-use ark_bls12_381::Fr;
+use ark_bls12_381::{Fr, FrConfig};
+use ark_ff::{Fp256, MontBackend, PrimeField};
 use ark_poly::{Polynomial};
 use ark_std::{One, UniformRand};
 use sha256::{digest};
+
+type FrElem = Fp256<MontBackend<FrConfig, 4>>;
+
+fn hash(element: &str, key: FrElem) -> Fr {
+    let concat = format!("{element}{key}");
+    let digest = digest(&concat);
+    Fr::from_le_bytes_mod_order(digest.as_bytes())
+}
 
 
 fn main() {
     // setup
     let mut x_rng = rand::thread_rng();
-    let a_pk = Fr::rand(&mut x_rng).to_string();
-    let b_pk = a_pk.clone();
+    let a_k = Fr::rand(&mut x_rng);
+    let b_k = a_k.clone();
 
     // Alice
     let a_data: Vec<&str> = vec!["a", "b", "c", "d", "e"];
-    let a_encoded : Vec<String> = a_data.iter().map(|x| {
-        let concat = format!("{a_pk}{x}");
-        digest(&concat)
-    }).collect();
+    let a_encoded: Vec<FrElem> = a_data.iter().map(|x| hash(x, a_k)).collect();
 
     // Bob
     let b_data: Vec<&str> = vec!["b", "c", "e", "f", "g", "h"];
-    let b_encoded : Vec<String> = b_data.iter().map(|x| {
-        let concat = format!("{b_pk}{x}");
-        digest(&concat)
-    }).collect();
+    let b_encoded: Vec<FrElem> = b_data.iter().map(|x| hash(x, b_k)).collect();
 
     // Steve
     let s_encoded_a = a_encoded.clone();
     let s_encoded_b = b_encoded.clone();
-    let s_encoded_a_set : HashSet<String> = s_encoded_a.into_iter().collect();
-    let s_encoded_b_set : HashSet<String> = s_encoded_b.into_iter().collect();
-    let intersection : HashSet<&String> = s_encoded_a_set.intersection(&s_encoded_b_set).collect();
+    let s_encoded_a_set: HashSet<FrElem> = s_encoded_a.into_iter().collect();
+    let s_encoded_b_set: HashSet<FrElem> = s_encoded_b.into_iter().collect();
+    let intersection: HashSet<&FrElem> = s_encoded_a_set.intersection(&s_encoded_b_set).collect();
     let intersection_size = intersection.len();
 
     // Alice
@@ -39,9 +42,9 @@ fn main() {
     // Bob
     let b_result = intersection_size;
 
-    // println!("{:?}", a_encoded);
-    // println!("{:?}", b_encoded);
-    // println!("{}", a_result);
+    println!("{:?}", a_encoded);
+    println!("{:?}", b_encoded);
+    println!("{}", a_result);
 }
 
 
