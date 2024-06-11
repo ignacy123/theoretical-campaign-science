b34e88c8bd9452d6da08ba57f351b08ac86f2e4b
Bartek Wacławik <bartek.waclawik@student.uj.edu.pl>
2024-06-11 14:42:49 +0200

    [web] Fix a bug in start script

--- web/start_release_https.sh
+++ web/start_release_https.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[ -f certfile.pem -a -f keyfile.pem ] || exit "Cannot locate cryptografic files"
+[ -f certfile.pem -a -f keyfile.pem ] || { echo "ERROR: Cannot locate cryptografic files" >&2; exit 1; }
 gunicorn --certfile certfile.pem --keyfile privkey.pem -w 2 -b 0.0.0.0:443 app:app
